c6e0bcb24d8ce48e98fc3fec2579ab6ffae9253b
Guilherme de jesus <gui-rock02@hotmail.com>
2021-12-13 12:24:18 -0300

    Atualização geral, crud completo implementado, funções de vizualizar, excluir, atualizar e criar implementadas

--- DotnetCoreGravarDadosWMySQLWF/DotnetCoreGravarDadosWMySQLWF/Form1.Designer.cs
+++ DotnetCoreGravarDadosWMySQLWF/DotnetCoreGravarDadosWMySQLWF/Form1.Designer.cs
@@ -29,13 +29,23 @@ namespace DotnetCoreGravarDadosWMySQLWF
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textNome = new System.Windows.Forms.TextBox();
-            this.textNumero = new System.Windows.Forms.TextBox();
+            this.textTelefone = new System.Windows.Forms.TextBox();
             this.textEmail = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.list_contatos = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBusca = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -50,7 +60,7 @@ namespace DotnetCoreGravarDadosWMySQLWF
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Location = new System.Drawing.Point(14, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 15);
             this.label2.TabIndex = 1;
@@ -59,7 +69,7 @@ namespace DotnetCoreGravarDadosWMySQLWF
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 102);
+            this.label3.Location = new System.Drawing.Point(12, 102);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(36, 15);
             this.label3.TabIndex = 2;
@@ -72,44 +82,125 @@ namespace DotnetCoreGravarDadosWMySQLWF
             this.textNome.Size = new System.Drawing.Size(245, 23);
             this.textNome.TabIndex = 3;
             // 
-            // textNumero
+            // textTelefone
             // 
-            this.textNumero.Location = new System.Drawing.Point(13, 76);
-            this.textNumero.Name = "textNumero";
-            this.textNumero.Size = new System.Drawing.Size(245, 23);
-            this.textNumero.TabIndex = 4;
+            this.textTelefone.Location = new System.Drawing.Point(14, 76);
+            this.textTelefone.Name = "textTelefone";
+            this.textTelefone.Size = new System.Drawing.Size(245, 23);
+            this.textTelefone.TabIndex = 4;
             // 
             // textEmail
             // 
-            this.textEmail.Location = new System.Drawing.Point(13, 121);
+            this.textEmail.Location = new System.Drawing.Point(12, 120);
             this.textEmail.Name = "textEmail";
             this.textEmail.Size = new System.Drawing.Size(245, 23);
             this.textEmail.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(140, 153);
+            this.button1.Location = new System.Drawing.Point(12, 159);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 34);
+            this.button1.Size = new System.Drawing.Size(245, 34);
             this.button1.TabIndex = 6;
             this.button1.Text = "Salvar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // list_contatos
+            // 
+            this.list_contatos.ContextMenuStrip = this.contextMenuStrip1;
+            this.list_contatos.HideSelection = false;
+            this.list_contatos.Location = new System.Drawing.Point(265, 32);
+            this.list_contatos.MultiSelect = false;
+            this.list_contatos.Name = "list_contatos";
+            this.list_contatos.Size = new System.Drawing.Size(584, 470);
+            this.list_contatos.TabIndex = 7;
+            this.list_contatos.UseCompatibleStateImageBehavior = false;
+            this.list_contatos.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.list_contatos_ItemSelectionChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(110, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.AccessibleDescription = "Excluir Item";
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(109, 22);
+            this.toolStripMenuItem1.Text = "Excluir";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Buscar na tabela";
+            // 
+            // textBusca
+            // 
+            this.textBusca.Location = new System.Drawing.Point(364, 5);
+            this.textBusca.Name = "textBusca";
+            this.textBusca.Size = new System.Drawing.Size(369, 23);
+            this.textBusca.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(739, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 24);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(245, 34);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Novo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 262);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(245, 37);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Deletar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 199);
+            this.ClientSize = new System.Drawing.Size(861, 514);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBusca);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.list_contatos);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textEmail);
-            this.Controls.Add(this.textNumero);
+            this.Controls.Add(this.textTelefone);
             this.Controls.Add(this.textNome);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tabela de Colaboradores";
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,9 +212,17 @@ namespace DotnetCoreGravarDadosWMySQLWF
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textNome;
-        private System.Windows.Forms.TextBox textNumero;
+        private System.Windows.Forms.TextBox textTelefone;
         private System.Windows.Forms.TextBox textEmail;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView list_contatos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBusca;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- DotnetCoreGravarDadosWMySQLWF/DotnetCoreGravarDadosWMySQLWF/Form1.cs
+++ DotnetCoreGravarDadosWMySQLWF/DotnetCoreGravarDadosWMySQLWF/Form1.cs
@@ -14,42 +14,351 @@ namespace DotnetCoreGravarDadosWMySQLWF
 {
     public partial class Form1 : Form
     {
-        MySqlConnection Conexao;
+        private MySqlConnection Conexao;
+        private string data_source = "datasource=localhost;username=root;password=root;database=db_agenda";
+
+        private int ?id_contato_selecionado = null;
 
         public Form1()
         {
             InitializeComponent();
+
+            list_contatos.View = View.Details;
+            list_contatos.LabelEdit = true;
+            list_contatos.AllowColumnReorder = true;
+            list_contatos.FullRowSelect = true;
+            list_contatos.GridLines = true;
+
+            list_contatos.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            list_contatos.Columns.Add("Nome", 150, HorizontalAlignment.Left);
+            list_contatos.Columns.Add("Email", 150, HorizontalAlignment.Left);
+            list_contatos.Columns.Add("Telefone", 150, HorizontalAlignment.Left);
+
+            carregar_contatos();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                string data_source = "datasource=localhost;username=root;password=root;database=db_agenda";
-                //Criar conexao com mysql
                 Conexao = new MySqlConnection(data_source);
-
-
-                string sql = "INSERT INTO Contato (nome, email, telefone) "
-                    + "VALUES " + 
-                    "('" + textNome.Text + "', '" + textEmail.Text + "', '" + textNumero.Text + "')";
-                //Executar Comando Insert
-
-
-                MySqlCommand comando = new MySqlCommand(sql, Conexao);
-
                 Conexao.Open();
 
-                comando.ExecuteReader();
+                MySqlCommand cmd = new MySqlCommand();
 
-                MessageBox.Show("Processo concluido, informação inserida");
+                cmd.Connection = Conexao;
+
+
+                if(id_contato_selecionado == null)
+                {
+                    //insert
+                    cmd.CommandText = "INSERT INTO Contato (nome, email, telefone) " +
+                                "VALUES" + "(@nome, @email, @telefone) ";
 
 
 
-            } catch(Exception ex)
+                    cmd.Parameters.AddWithValue("@nome", textNome.Text);
+                    cmd.Parameters.AddWithValue("@email", textEmail.Text);
+                    cmd.Parameters.AddWithValue("@telefone", textTelefone.Text);
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("informação inserida com sucesso", "Concluido!", 
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+                    //update
+                    cmd.CommandText = "UPDATE Contato SET " +
+                        "nome=@nome, email=@email, telefone=@telefone " + 
+                        "WHERE id=@id ";
+
+
+
+                    cmd.Parameters.AddWithValue("@nome", textNome.Text);
+                    cmd.Parameters.AddWithValue("@email", textEmail.Text);
+                    cmd.Parameters.AddWithValue("@telefone", textTelefone.Text);
+                    cmd.Parameters.AddWithValue("@id", id_contato_selecionado);
+
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("informações atualizadas com sucesso", "Concluido!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+
+                limpar_formulario();
+
+                carregar_contatos();
+
+                /* jeito desorganizado de se fazer >>
+                 * string sql = "INSERT INTO Contato (nome, email, telefone) "
+                     + "VALUES " + 
+                     "('" + textNome.Text + "', '" + textEmail.Text + "', '" + textNumero.Text + "')";
+                 //Executar Comando Insert
+
+
+                 MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                 Conexao.Open();
+
+                 comando.ExecuteReader();
+
+                 MessageBox.Show("Processo concluido, informação inserida");*/
+
+
+            }
+            catch (MySqlException ex) 
             {
-                MessageBox.Show(ex.Message);
-            } finally
+                MessageBox.Show("Erro" + ex.Number + " Ocorreu um: " + ex.Message,
+                               "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro  Houve um: " + ex.Message,
+                                "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = Conexao;
+
+                cmd.CommandText = "SELECT * FROM Contato WHERE nome LIKE @q OR email LIKE @q";
+
+                
+
+                cmd.Parameters.AddWithValue("@q", "%" + textBusca.Text + "%");
+                cmd.Prepare();
+
+
+
+
+                //  string q = "'%" + textBusca.Text + "%'";
+
+
+                //Criar conexao com mysql
+                //   Conexao = new MySqlConnection(data_source);
+
+
+                /*    string sql = "SELECT * FROM Contato WHERE nome LIKE" + q + "OR email LIKE" + q;
+
+                    Conexao.Open();  
+
+                    //Executar Comando Insert
+                    MySqlCommand comando = new MySqlCommand(sql, Conexao);
+                */
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                list_contatos.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+
+                    // var linha_listview = new ListViewItem(row);
+
+                    list_contatos.Items.Add(new ListViewItem(row));
+                }
+            }
+
+
+
+
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erro" + ex.Number + " Ocorreu um: " + ex.Message,
+                               "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro  Houve um: " + ex.Message,
+                                "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void carregar_contatos()
+        {
+            try
+            {
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = Conexao;
+                cmd.CommandText = "SELECT * FROM Contato ORDER BY id ASC";
+                cmd.Prepare();
+
+
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                list_contatos.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+
+
+                    list_contatos.Items.Add(new ListViewItem(row));
+                }
+            }
+
+
+
+
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erro" + ex.Number + " Ocorreu um: " + ex.Message,
+                               "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro  Houve um: " + ex.Message,
+                                "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void list_contatos_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            ListView.SelectedListViewItemCollection itens_selecionados = list_contatos.SelectedItems;
+            
+            foreach(ListViewItem item in itens_selecionados)
+            {
+                id_contato_selecionado = Convert.ToInt32(item.SubItems[0].Text);
+
+                textNome.Text = item.SubItems[1].Text;
+                textEmail.Text = item.SubItems[2].Text;
+                textTelefone.Text = item.SubItems[3].Text;
+
+                button4.Visible = true;
+
+                //MessageBox.Show("id selecionado = " + id_contato_selecionado);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            limpar_formulario();
+        }
+
+        private void limpar_formulario()
+        {
+            id_contato_selecionado = null;
+
+            textNome.Text = string.Empty;
+            textEmail.Text = "";
+            textTelefone.Text = "";
+
+            textNome.Focus();
+
+            button4.Visible = false;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            excluir_contato();
+           // MessageBox.Show("Excluindo id = " + id_contato_selecionado);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            excluir_contato();
+        }
+
+        private void excluir_contato()
+        {
+            try
+            {
+
+
+                DialogResult conf = MessageBox.Show("Deseja excluir o item selecionado?",
+                                "Excluir item selecionado?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (conf == DialogResult.Yes)
+                {
+                    Conexao = new MySqlConnection(data_source);
+                    Conexao.Open();
+
+                    MySqlCommand cmd = new MySqlCommand();
+
+                    cmd.Connection = Conexao;
+
+                    cmd.CommandText = "DELETE FROM Contato WHERE id=@id ";
+
+
+                    cmd.Parameters.AddWithValue("@id", id_contato_selecionado);
+
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+
+                    //excluir no db
+                    MessageBox.Show("registro excluido com sucesso", "Concluido!",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    carregar_contatos();
+
+                    button4.Visible = false;
+
+                    limpar_formulario();
+                }
+
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erro" + ex.Number + " Ocorreu um: " + ex.Message,
+                               "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro  Houve um: " + ex.Message,
+                                "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
             {
                 Conexao.Close();
             }
