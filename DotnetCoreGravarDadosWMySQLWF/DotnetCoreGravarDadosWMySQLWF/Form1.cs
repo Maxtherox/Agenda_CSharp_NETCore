ebfb18444a1462374cf4b83556fc794180b4d955
Guilherme de jesus <gui-rock02@hotmail.com>
2021-12-12 21:30:00 -0300

    Adição do projeto ao Github, função de salvar pronta.

---
+++ DotnetCoreGravarDadosWMySQLWF/DotnetCoreGravarDadosWMySQLWF/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace DotnetCoreGravarDadosWMySQLWF
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection Conexao;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data_source = "datasource=localhost;username=root;password=root;database=db_agenda";
+                //Criar conexao com mysql
+                Conexao = new MySqlConnection(data_source);
+
+
+                string sql = "INSERT INTO Contato (nome, email, telefone) "
+                    + "VALUES " + 
+                    "('" + textNome.Text + "', '" + textEmail.Text + "', '" + textNumero.Text + "')";
+                //Executar Comando Insert
+
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                Conexao.Open();
+
+                comando.ExecuteReader();
+
+                MessageBox.Show("Processo concluido, informação inserida");
+
+
+
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            } finally
+            {
+                Conexao.Close();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # API_Csharp_WF__MySQL
-Minha primeira criação de uma aplicação API super simples de cadastro e conectando ela ao MySQL
+Minha primeira criação de uma aplicação API super simples de cadastro em C# e conectando ela ao MySQL
